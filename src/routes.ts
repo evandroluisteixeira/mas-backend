e32249cc47bc7a3678f3bf11b5e90b460e3684ff
Evandro Luis Teixeira <35399118+evandroluisteixeira@users.noreply.github.com>
2021-05-02 21:43:44 -0300

    feature/add-migrations
    feat: add new migrations files

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,16 @@
-import {request, Router} from 'express';
+import {Request, Router, Response, response} from 'express';
+import {UserController} from './controller/UserController';
+import {ActivyController} from './controller/ActivyController';
+import {CourseUnitController} from './controller/CourseUnitController';
+
+const userController = new UserController()
+const activyController = new ActivyController()
+const courseUnitController = new CourseUnitController()
 
 const routes = Router();
 
-routes.get('/user' , (request, response) =>{
-    const{nome, idade}=request.query;
-
-    response.json({
-        idade,
-        nome, 
-        message: "Idade e Nome"
-    })
-
-})
+routes.post('/user', userController.create);
+routes.post('/activy', activyController.create);
+routes.post('/courseunit', courseUnitController.create);
 
 export default routes;
